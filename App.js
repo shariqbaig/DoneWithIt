1ced15dcd609e73469cd0a725a3baf5d5f38e9e4
Shariq Baig <shariqbaig97@hotmail.com>
2020-06-11 03:03:52 +0500

    AppButton work

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppButton title="Login" onPress={() => console.log("Tapped")} />
+    </View>
+  );
 }
